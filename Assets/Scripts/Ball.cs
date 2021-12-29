1dcb7aa59d8ee3e56ddc493854d82cae1fad1a5a
raassh-23 <muh.nur.abdurrauf@gmail.com>
2021-12-29 21:19:16 +0700

    add problem 2

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random=UnityEngine.Random;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2d;
+
+    [SerializeField]
+    private float speed = 5;
+
+    private Vector2 direction;
+
+    private void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        direction = Random.insideUnitCircle.normalized;
+    }
+
+    private void Update() {
+        MoveBall();
+    }
+
+    void MoveBall()
+    {
+        rigidbody2d.velocity = direction * speed;
+    }
+}
